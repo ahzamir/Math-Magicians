45a7a0d91c972495991bf0bd1bdc8d0d7bd54b1c
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-09 18:50:54 +0430

    add the eventHandler on each Button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const onClickHandler = (e) => {
+  const buttonName = e.target.innerText;
+  console.log(buttonName);
+};
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
@@ -7,25 +11,25 @@ class Calculator extends React.Component {
       <div className="calculator-div">
         <input className="displayNum" type="text" id="result" placeholder="0" disabled />
         <div className="calculatorButtons">
-          <button className="color-first" type="submit">AC</button>
-          <button className="color-first" type="submit">+/-</button>
-          <button className="color-first" type="submit">%</button>
-          <button className="color-first" type="submit">/</button>
-          <button className="color-second" type="submit">7</button>
-          <button className="color-second" type="submit">8</button>
-          <button className="color-second" type="submit">9</button>
-          <button className="color-first" type="submit">*</button>
-          <button className="color-second" type="submit">4</button>
-          <button className="color-second" type="submit">5</button>
-          <button className="color-second" type="submit">6</button>
-          <button className="color-first" type="submit">-</button>
-          <button className="color-second" type="submit">1</button>
-          <button className="color-second" type="submit">2</button>
-          <button className="color-second" type="submit">3</button>
-          <button className="color-first" type="submit">+</button>
-          <button className="color-second zero" type="submit">0</button>
-          <button className="color-second" type="submit">.</button>
-          <button className="color-first" type="submit">=</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>AC</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>+/-</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>%</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>÷</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>7</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>8</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>9</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>x</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>4</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>5</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>6</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>-</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>1</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>2</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>3</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>+</button>
+          <button className="color-second zero" type="submit" onClick={onClickHandler}>0</button>
+          <button className="color-second" type="submit" onClick={onClickHandler}>.</button>
+          <button className="color-first" type="submit" onClick={onClickHandler}>=</button>
         </div>
       </div>
     );
