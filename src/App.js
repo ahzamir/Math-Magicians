39331f8df67dd8633e071cd95091b9741dc87eab
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-09 10:08:03 +0430

    solving linters

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
       <Calculator />
     );
   }
-} 
+}
 
 export default App;
