e8dd098cb160a2102e8ab8cedc737d504a477c61
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-09 10:06:55 +0430

    changing app.js to class build func

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <Calculator />
-);
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+} 
 
 export default App;
