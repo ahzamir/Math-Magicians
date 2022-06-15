4f672ac1c228462d2bcc26c5ec7ee6428ae503cd
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-16 00:57:44 +0430

    write tests for calculate.js

---
+++ src/__tests__/calculate.test.js
@@ -0,0 +1,54 @@
+import calculate from "../logic/calculate";
+
+test('Test the functionality for AC button', () => {
+  const obj = {
+    total: 10,
+    next: 20,
+    operation: '+',
+  };
+  const result = calculate(obj, 'AC');
+  expect(result.total).toBe(null);
+  expect(result.next).toBe(null);
+  expect(result.operation).toBe(null);
+});
+
+test('Test the functionality for pressing 0 button', () => {
+  const obj = {
+    total: null,
+    next: 0,
+    operation: '+',
+  };
+  const result = calculate(obj, '0');
+  expect(result.total).toBe(null);
+});
+
+test('Test the functionality for pressing the buttons whithout operation', () => {
+  const obj = {
+    total: 10,
+    next: 20,
+    operation: null,
+  };
+  const result = calculate(obj, '1');
+  expect(result.next).toBe('201');
+  expect(result.total).toBe(null);
+});
+
+test('Test the functionality for pressing the 0 button after + operation', () => {
+  const obj = {
+    total: 10,
+    next: 0,
+    operation: '+',
+  };
+  const result = calculate(obj, '0');
+  expect(result.total).toBe(10);
+});
+
+test('Test the functionality for pressing the = button after an operation', () => {
+  const obj = {
+    total: 10,
+    next: 5,
+    operation: '-',
+  };
+  const result = calculate(obj, '=');
+  expect(result.total).toBe('5');
+});
